a83a367817d6e7013c4ff5e60a02b20a731c5efd
MSBeni <andrei.sokurov.bitco@gmail.com>
2022-09-19 10:51:24 -0700

    Added cleaned version

--- workspace/first_solidity_project/contracts/hello_world.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity ^0.5.0;
-
-contract HelloWorld {
-string private helloMessage = "Hello world";
-function getHelloMessage() public view returns
-(string memory) {
-return helloMessage;
-}
-}
